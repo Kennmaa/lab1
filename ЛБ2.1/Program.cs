6d485bc36c2bda7b33ec31579fa4aba87aa27c98
Анастасия САмойлова <nastenapy1618@gmail.com>
2024-10-17 20:19:48 +0200

    Лб2

---
+++ ЛБ2.1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите значения x: ");
+double x = Convert.ToDouble(Console.ReadLine());
+double f = 1e-10;
+if (Math.Abs(Math.Cos(x)) > f)
+{
+    double y = 2 * Math.Tan(x);
+    Console.WriteLine("Значение функции: " + y);
+}
+else
+{
+    Console.WriteLine("Функция не определена для x = " + x);
+}
---
+++ ЛБ2.2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите значения x: ");
+double x = Convert.ToDouble(Console.ReadLine());
+if (x != 1)
+{
+    double y = 3 / (x - 1);
+    Console.WriteLine("Значение функции: " + y);
+}
+else
+{
+    Console.WriteLine("Функция не определена для x = " + x);
+}
+
